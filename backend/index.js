0516731b534a9f8ce6976b1ea880552ba39dde73
Syu125 <yusophia2011@gmail.com>
2025-03-07 15:15:03 -0800

    Fix backend linter version

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,8 +10,8 @@ app.use(express.json());
 app.use(cors());
 
 // Test Route
-app.get("/", (req, res) => {
-  res.json({ message: "Backend is running!" });
+app.get('/', (req, res) => {
+  res.json({ message: 'Backend is running!' });
 });
 
 app.listen(PORT, () => {
