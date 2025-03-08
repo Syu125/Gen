73d89bb39a1b3b550c781324e243baf3bfcff531
Syu125 <yusophia2011@gmail.com>
2025-03-07 16:47:22 -0800

    Added background and card

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,23 @@
-"use client";
-import { useEffect, useState } from "react";
+'use client';
+import { useEffect, useState } from 'react';
+
+import styles from './page.module.css';
 
 export default function Home() {
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch("http://localhost:5000") // Call backend
+    fetch('http://localhost:5000') // Call backend
       .then((res) => res.json())
       .then((data) => setMessage(data.message))
       .catch((err) => console.error(err));
   }, []);
 
-  return <div className="p-4 text-xl">{message || "Loading..."}</div>;
+  return (
+    <main className={styles.page}>
+      <div className={styles.contentFrame}>
+        <div className={styles.content}></div>
+      </div>
+    </main>
+  );
 }
