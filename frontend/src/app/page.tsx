a93870fd46570b734f521e7e184580288bd794bc
Syu125 <yusophia2011@gmail.com>
2025-03-08 11:00:38 -0800

    Comment out backend connection for now

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,14 +6,14 @@ import Button from '../components/button/Button';
 import styles from './page.module.css';
 
 export default function Home() {
-  const [message, setMessage] = useState('');
+  // const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch('http://localhost:5000') // Call backend
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch((err) => console.error(err));
-  }, []);
+  // useEffect(() => {
+  //   fetch('http://localhost:5000') // Call backend
+  //     .then((res) => res.json())
+  //     .then((data) => setMessage(data.message))
+  //     .catch((err) => console.error(err));
+  // }, []);
 
   return (
     <main className={`bg-custom ${styles.page}`}>
