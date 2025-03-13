9961da90925c765b2c133bfa6e2dd6a141167da3
Syu125 <yusophia2011@gmail.com>
2025-03-13 15:14:25 -0700

    Created main action component

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,1 +1,11 @@
-export default function Dashboard() {}
+import styles from './page.module.css';
+
+import MainAction from '@/components/main-action/MainAction';
+
+export default function Dashboard() {
+  return (
+    <main className={styles.page}>
+      <MainAction></MainAction>
+    </main>
+  );
+}
